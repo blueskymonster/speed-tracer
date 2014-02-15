ba50487cded49b1f37323c800782c331185844f0
Elliot Godzich <elliot.godzich@originate.com>
2014-02-15 13:47:29 -0800

    help and debug options added. Syntax style homogenizend.

--- SpeedTracer.cpp
+++ SpeedTracer.cpp
@@ -16,25 +16,41 @@ Mat fgMask; //current foreground mask learned by MOG method
 Ptr<BackgroundSubtractor> pMOG; //MOG Background subtractor
 int keyboard;
 
+//debug variables
+bool debug;
+Mat backgroundModel;
+
 //function declarations
 void help();
 void videoProcessLoop();
 
-void help()
-{
-  cout << "Bro, this program doesn't take parameters." << endl;
+void help() {
+  cout << "Usage: ./SpeedTracer [--help | --debug]"            << endl
+       << "Options:"                                           << endl
+       << "--help                          Shows this message" << endl
+       << "--debug       Activates debug windows and messages" << endl;
 }
 
-int main(int argc, char* argv[])
-{
-  if(argc != 1)
-  {
+int main(int argc, char* argv[]) {
+  string helpOption = "--help";
+  string debugOption = "--debug";
+
+  debug = argc == 2 && argv[1] == debugOption;
+  if (!debug && (argc > 2 || (argc == 2 && argv[1] == helpOption))) {
     help();
     return EXIT_FAILURE;
   }
 
   //create GUI window
   namedWindow("Speed Tracer");
+
+  //debug windows
+  if (debug) {
+    namedWindow("camera"); //shows the raw camera feed
+    //TODO: make a window that shows the currently learned background model
+    namedWindow("mask"); //shows the foreground mask
+    //TODO: make a window that shows the masked camera feed
+  }
 
   //create background subtractor object
   pMOG = createBackgroundSubtractorMOG();
@@ -47,24 +63,20 @@ int main(int argc, char* argv[])
   return EXIT_SUCCESS;
 }
 
-void videoProcessLoop()
-{ 
+void videoProcessLoop() {
   //initialize video capture from whatever webcam is default
   VideoCapture cap(0);
   
   //verify that the feed has been acquired properly
-  if(!cap.isOpened())
-  {
+  if (!cap.isOpened()) {
     cerr << "Failed to acquire camera" << endl;
     exit(EXIT_FAILURE);
   }
 
   //start the video loop. 'q' to quit
-  while((char)keyboard != 'q')
-  {
+  while((char)keyboard != 'q') {
     //read frame from camera, quit if this fails
-    if(!cap.read(cameraFrame))
-    {
+    if (!cap.read(cameraFrame)) {
       cerr << "Failed to read frame from camera" << endl;
       exit(EXIT_FAILURE);
     }
@@ -75,6 +87,12 @@ void videoProcessLoop()
     //copy only foreground pixels to the display image buffer
     cameraFrame.copyTo(displayFrame, fgMask);
     
+    //display debug windows
+    if (debug) {
+      imshow("camera", cameraFrame);
+      imshow("mask", fgMask);
+    }
+
     //display image buffer
     imshow("Speed Tracer", displayFrame);
 
