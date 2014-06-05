e905801fb97e3fb32ed540876705b0a9c9c27f12
Elliot Godzich <elliot@socialwire.com>
2014-06-04 23:33:01 -0700

    Temporarily making code horrible for the sake of getting this to work with flea cam for the weekend

--- SpeedTracer.cpp
+++ SpeedTracer.cpp
@@ -2,6 +2,8 @@
 #include <opencv2/video/background_segm.hpp>
 #include <opencv2/core/utility.hpp>
 
+#include <flycapture/FlyCapture2.h>
+
 #include <stdio.h>
 
 #include <iostream>
@@ -27,7 +29,7 @@ Mat backgroundModel;
 
 //function declarations
 void calibrateRunningCamera();
-void videoProcessLoop();
+void videoProcessLoop(bool);
 
 int main(int argc, char* argv[]) {
   const String commandLineKeys =
@@ -36,6 +38,7 @@ int main(int argc, char* argv[]) {
     "{calibrationScript |../do-nothing.sh      | sets the command to run to calibrate camera                                       }"
     "{learningFrames lf |0                     | sets number of frames to use to learn the background. (0 for continuous learning) }"
     "{learningRate lr   |-1                    | sets the background subtractors learning rate (between 0 and 1, negative for auto)}"
+    "{fleaCam           |                      | use the point grey flea cam drivers                                               }"
     ;
 
   CommandLineParser clParser(argc, argv, commandLineKeys);
@@ -51,15 +54,17 @@ int main(int argc, char* argv[]) {
 
   //debug windows
   if (debug) {
-    namedWindow("camera"); //shows the raw camera feed
-    namedWindow("background model"); //shows the current background model
-    namedWindow("mask"); //shows the foreground mask
+    namedWindow("camera", WINDOW_NORMAL); //shows the raw camera feed
+    namedWindow("background model", WINDOW_NORMAL); //shows the current background model
+    namedWindow("mask", WINDOW_NORMAL); //shows the foreground mask
     //TODO: make a window that shows the masked camera feed
   } else {
     cout << "Running in fullscreen mode" << endl;
     setWindowProperty("Speed Tracer", WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
   }
 
+  //What type of camera are we using?
+  bool useFleaCam = clParser.has("fleaCam");
 
   //get calibration script from command line args
   calibrationScript = clParser.get<String>("calibrationScript");
@@ -74,7 +79,7 @@ int main(int argc, char* argv[]) {
   pMOG = createBackgroundSubtractorMOG2();
 
   //here's where the magic happens
-  videoProcessLoop();
+  videoProcessLoop(useFleaCam);
 
   destroyAllWindows();
 
@@ -86,28 +91,71 @@ void calibrateRunningCamera() {
   system(calibScript);
 }
 
-void videoProcessLoop() {
-  //initialize video capture from whatever webcam is default
-  VideoCapture cap(0);
+void videoProcessLoop(bool useFleaCam) {
 
-  //verify that the feed has been acquired properly
-  if (!cap.isOpened()) {
-    cerr << "Failed to acquire camera" << endl;
-    exit(EXIT_FAILURE);
+  // //initialize video capture from whatever webcam is default
+  // VideoCapture cap(0);
+
+  // //verify that the feed has been acquired properly
+  // if (!cap.isOpened()) {
+  //   cerr << "Failed to acquire camera" << endl;
+  //   exit(EXIT_FAILURE);
+  // }
+
+  // // Force camera on for calibration
+  // cap.read(cameraFrame);
+
+  FlyCapture2::Error error;
+  FlyCapture2::Camera camera;
+  FlyCapture2::CameraInfo camInfo;
+
+  error = camera.Connect(0);
+  if (error != FlyCapture2::PGRERROR_OK) {
+    cout << "Failed to connect to camera" << endl;
+    return;
   }
 
-  // Force camera on for calibration
-  cap.read(cameraFrame);
+  error = camera.GetCameraInfo(&camInfo);
+  if (error != FlyCapture2::PGRERROR_OK) {
+    cout << "Failed to get camera info from camera" << endl;
+    return;
+  }
+
+  cout << camInfo.vendorName << " " << camInfo.modelName << " " << camInfo.serialNumber << endl;
+
+  error = camera.StartCapture();
+  if (error == FlyCapture2::PGRERROR_ISOCH_BANDWIDTH_EXCEEDED) {
+    cout << "Bandwidth exceeded" << endl;
+    return;
+  } else if (error != FlyCapture2::PGRERROR_OK) {
+    cout << "Failed to start image capture" << endl;
+    return;
+  }
+
+  FlyCapture2::Image rawImage;
+  FlyCapture2::Image rgbImage;
+  unsigned int rowBytes;
+
   calibrateRunningCamera();
 
   //start the video loop. 'q' to quit
   while((char)keyboard != 'q') {
-    //read frame from camera, quit if this fails
-    if (!cap.read(cameraFrame)) {
-      cerr << "Failed to read frame from camera" << endl;
-      exit(EXIT_FAILURE);
+    // //read frame from camera, quit if this fails
+    // if (!cap.read(cameraFrame)) {
+    //   cerr << "Failed to read frame from camera" << endl;
+    //   exit(EXIT_FAILURE);
+    // }
+    // flip(cameraFrame, cameraFrame, 1);
+    error = camera.RetrieveBuffer(&rawImage);
+    if (error != FlyCapture2::PGRERROR_OK) {
+      cout << "capture error" << endl;
+      continue;
     }
-    flip(cameraFrame, cameraFrame, 1);
+    rawImage.Convert(FlyCapture2::PIXEL_FORMAT_BGR, &rgbImage);
+
+    rowBytes = (double)rgbImage.GetReceivedDataSize()/(double)rgbImage.GetRows();
+    cameraFrame = Mat(rgbImage.GetRows(), rgbImage.GetCols(), CV_8UC3, rgbImage.GetData(), rowBytes);
+
 
     //update the background model
     if (learningFrames == 0 || frame < learningFrames) {
@@ -134,6 +182,8 @@ void videoProcessLoop() {
     keyboard = waitKey(30);
   }
 
-  //let the webcam go free
-  cap.release();
+  // //let the webcam go free
+  // cap.release();
+  camera.StopCapture();
+  camera.Disconnect();
 }
